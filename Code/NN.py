828258b770a427e8089af3213f05c1c145b010d8
Marcelo Shao <haochongshao@gmail.com>
2025-03-26 22:25:12 +0000

    Enhance data loading and preprocessing: add load_split_data function, update dataset extraction logic, and improve confusion matrix visualization with classification report

--- Code/NN.py
+++ Code/NN.py
@@ -1,13 +1,15 @@
-from load_data import load_data
-from plot_confusion import plot_history, plot_confusion
+from load_data import load_data,load_split_data
+from plot_confusion import plot_confusion, generate_classification_report, plot_history
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
+from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras.utils import to_categorical
+from keras.models import load_model
 
 
 def NN(dropout_rate=0.2, input_dim=6, num_layers=3, initial_neurons=64):
@@ -22,34 +24,48 @@ def NN(dropout_rate=0.2, input_dim=6, num_layers=3, initial_neurons=64):
 
 # Main code
 # Load data
-(X_train_1, y_train_1), (X_test_1, y_test_1) = load_data('Data/Stable_1.csv', label_columns='Labels', test_size=0.2)
-(X_train_2, y_train_2), (X_test_2, y_test_2) = load_data('Data/Stable_2.csv', label_columns='Labels', test_size=0)
-
-X_train = X_train_1
-X_test = X_test_1
-y_train = y_train_1
-y_test = y_test_1
+# Load data (assumed to be already normalized in load_data)
+(X_train_1, y_train_1), (X_test_1, y_test_1) = load_split_data('Data/Stable_1.csv')
+(X_train_2, y_train_2),(X_test_2, y_test_2) = load_split_data('Data/Stable_2.csv')
 
 # If the CSV includes a timestamp as the first column, drop it to keep only the 6 feature dimensions.
-X_train = X_train[:, 1:]
-X_test = X_test[:, 1:]
+X_train_1 = X_train_1[:, 1:]
+X_test_1 = X_test_1[:, 1:]
+X_train_2 = X_train_2[:, 1:]
+X_test_2 = X_test_2[:, 1:]
 
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+combine_X_train = np.concatenate((X_train_1, X_train_2), axis=0)
+combine_y_train = np.concatenate((y_train_1, y_train_2), axis=0)
+combine_X_test = np.concatenate((X_test_1, X_test_2), axis=0)
+combine_y_test = np.concatenate((y_test_1, y_test_2), axis=0)
+
+# Convert labels to one-hot encoding
+y_train_1 = to_categorical(y_train_1)
+y_test_1 = to_categorical(y_test_1)
 y_train_2 = to_categorical(y_train_2)
+y_test_2 = to_categorical(y_test_2)
+combine_y_train = to_categorical(combine_y_train)
+combine_y_test = to_categorical(combine_y_test)
+
+
 
 
 # NN
-model = NN(dropout_rate=0.5, input_dim=X_train.shape[1], num_layers=5, initial_neurons=256)
-model.compile(loss='binary_crossentropy', optimizer=Adam(0.001), metrics=['accuracy'])
-history = model.fit(X_train, y_train, epochs=500, batch_size=32, validation_data=(X_test, y_test))
+model = NN(dropout_rate=0.5, input_dim=6, num_layers=4, initial_neurons=512)
+model.compile(loss="categorical_crossentropy", optimizer=Adam(0.001), metrics=['categorical_accuracy'])
+history = model.fit(X_train_1, y_train_1, epochs=125, batch_size=8,validation_data=(X_test_2, y_test_2))
+
+model_path = 'Model/nn_model.h5'
+model = load_model(model_path)
+
 
 # Plot confusion matrix on unseen data
-plot_confusion(np.argmax(y_train_2, axis=1), np.argmax(model.predict(X_train_2[:,1:]), axis=1))
+plot_confusion(np.argmax(y_test_2, axis=1), np.argmax(model.predict(X_test_2), axis=1), label_path='Data/Stable_label_1.csv')
+generate_classification_report(np.argmax(y_test_2, axis=1), np.argmax(model.predict(X_test_2), axis=1), label_path='Data/Stable_label_1.csv')
 
 # Plot history
-plot_history(history)
+plot_history(history,acc='categorical_accuracy')
 
 #save model
-model.save('Model/nn_model.h5')
-print("Model saved")
+#model.save('Model/nn_model.h5')
+#print("Model saved")
--- Code/train_svm.py
+++ Code/train_svm.py
@@ -1,31 +1,51 @@
-from load_data import load_data
-from plot_confusion import plot_confusion
+from load_data import load_data, load_split_data
+from plot_confusion import plot_confusion, generate_classification_report
 from sklearn.svm import SVC
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
 # Load data (assumed to be already normalized in load_data)
-(X_train, y_train), (X_test, y_test) = load_data('Stable_dataset.csv', label_columns='Labels', test_size=0.2)
+(X_train_1, y_train_1), (X_test_1, y_test_1) = load_split_data('Data/Stable_1.csv')
+(X_train_2, y_train_2),(X_test_2, y_test_2) = load_split_data('Data/Stable_2.csv')
 
 # If the CSV includes a timestamp as the first column, drop it to keep only the 6 feature dimensions.
-X_train = X_train[:, 1:]
-X_test = X_test[:, 1:]
-print("First 5 rows of X_train:")
-print(X_train[0:5, :])
-print("First 5 labels of y_train:")
-print(y_train[0:5])
+X_train_1 = X_train_1[:, 1:]
+X_test_1 = X_test_1[:, 1:]
+X_train_2 = X_train_2[:, 1:]
+X_test_2 = X_test_2[:, 1:]
+
+combine_X_train = np.concatenate((X_train_1, X_train_2), axis=0)
+combine_y_train = np.concatenate((y_train_1, y_train_2), axis=0)
+combine_X_test = np.concatenate((X_test_1, X_test_2), axis=0)
+combine_y_test = np.concatenate((y_test_1, y_test_2), axis=0)
 
 # Train the SVM classifier
-classifier = SVC(kernel='linear', random_state=42, probability=True).fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
+#classifier = SVC(kernel='rbf', random_state=42, probability=True).fit(combine_X_train, combine_y_train)
+#y_pred = classifier.predict(combine_X_test)
+#y_pred_2 = classifier.predict(X_test_2)
 
-# Save the model so it can be reused in real-time prediction
-with open('Model/new_svm_model.pkl', 'wb') as f:
-    pickle.dump(classifier, f)
-    
-# evaluate
-plot_confusion(y_test, y_pred)
+combine_X_train_1 = np.concatenate((X_train_1, X_test_1), axis=0)
+combine_y_train_1 = np.concatenate((y_train_1, y_test_1), axis=0)
+combine_X_train_2 = np.concatenate((X_train_2, X_test_2), axis=0)
+combine_y_train_2 = np.concatenate((y_train_2, y_test_2), axis=0)
 
+# LOSO-CV: Train on participant 1's data, test on participant 2's data
+classifier_1 = SVC(kernel='rbf', random_state=42, probability=True).fit(combine_X_train_1, combine_y_train_1)
+y_pred_1 = classifier_1.predict(combine_X_train_2)
+plot_confusion(combine_y_train_2, y_pred_1, label_path='Data/Stable_label_1.csv')
+generate_classification_report(combine_y_train_2, y_pred_1, label_path='Data/Stable_label_1.csv')
+
+# LOSO-CV: Train on participant 2's data, test on participant 1's data
+classifier_2 = SVC(kernel='rbf', random_state=42, probability=True).fit(combine_X_train_2, combine_y_train_2)
+y_pred_2 = classifier_2.predict(combine_X_train_1)
+plot_confusion(combine_y_train_1, y_pred_2, label_path='Data/Stable_label_2.csv')
+generate_classification_report(combine_y_train_1, y_pred_2, label_path='Data/Stable_label_2.csv')
+
+# Save the models so they can be reused in real-time prediction
+# with open('Model/svm_model_1.pkl', 'wb') as f:
+#     pickle.dump(classifier_1, f)
+# with open('Model/svm_model_2.pkl', 'wb') as f:
+#     pickle.dump(classifier_2, f)
 
 
